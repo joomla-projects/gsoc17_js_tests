8f66209362219d2bd400c05aa8e96385ca62f744
Supun94 <supunpramoda@gmail.com>
2017-08-09 03:15:39 +0530

    Remove span in permission

--- media/system/js/fields/permissions.js
+++ media/system/js/fields/permissions.js
@@ -9,133 +9,131 @@
  */
 Joomla = window.Joomla || {};
 
-(function sendPermissions(event, Joomla) {
-	// set the icon while storing the values
-	var icon = document.getElementById('icon_' + this.id);
-	icon.removeAttribute('class');
-	icon.setAttribute('style', 'background: url(../media/system/images/modal/spinner.gif); display: inline-block; width: 16px; height: 16px');
+(function(Joomla) {
 
-	//get values and prepare GET-Parameter
-	var asset = 'not';
-	var component = getUrlParam('component');
-	var extension = getUrlParam('extension');
-	var option    = getUrlParam('option');
-	var view      = getUrlParam('view');
-	var title     = component;
-	var value     = this.value;
-	var context   = '';
+	window.sendPermissions = function(event) {
+		// set the icon while storing the values
+		var icon = document.getElementById('icon_' + this.id);
+		icon.removeAttribute('class');
+		icon.setAttribute('class', 'fa fa-spinner fa-spin');
 
-	if (document.getElementById('jform_context')){
-		context = document.getElementById('jform_context').value;
-		context = context.split('.')[0];
-	}
+		//get values and prepare GET-Parameter
+		var asset = 'not';
+		var component = Joomla.getUrlParam('component');
+		var extension = Joomla.getUrlParam('extension');
+		var option    = Joomla.getUrlParam('option');
+		var view      = Joomla.getUrlParam('view');
+		var title     = component;
+		var value     = this.value;
+		var context   = '';
 
-	if (option == 'com_config' && component == false && extension == false)
-	{
-		asset = 'root.1';
-	}
-	else if (extension == false && view == 'component'){
-		asset = component;
-	}
-	else if (context){
-		if (view == 'group') {
-			asset = context + '.fieldgroup.' + getUrlParam('id');
+		if (document.getElementById('jform_context')){
+			context = document.getElementById('jform_context').value;
+			context = context.split('.')[0];
 		}
-		else {
-			asset = context + '.field.' + getUrlParam('id');
-		}
-		title = document.getElementById('jform_title').value;
-	}
-	else if (extension != false && view != false){
-		asset = extension + '.' + view + '.' + getUrlParam('id');
-		title = document.getElementById('jform_title').value;
-	}
-	else if (extension == false && view != false){
-		asset = option + '.' + view + '.' + getUrlParam('id');
-		title = document.getElementById('jform_title').value;
-	}
 
-	var id = this.id.replace('jform_rules_', '');
-	var lastUnderscoreIndex = id.lastIndexOf('_');
-
-	var permission_data = {
-		comp   : asset,
-		action : id.substring(0, lastUnderscoreIndex),
-		rule   : id.substring(lastUnderscoreIndex + 1),
-		value  : value,
-		title  : title
-	};
-
-	// Remove js messages, if they exist.
-	Joomla.removeMessages();
-
-	// doing ajax request
-    var request = new XMLHttpRequest();
-    request.open("POST", document.getElementById('permissions-sliders').getAttribute('data-ajaxuri'), true);
-    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    request.send('')
-	.fail(function (jqXHR, textStatus, error) {
-		// Remove the spinning icon.
-		icon.removeAttribute('style');
-
-		Joomla.renderMessages(Joomla.ajaxErrorsMessages(jqXHR, textStatus, error));
-
-		window.scrollTo(0, 0);
-
-		icon.setAttribute('class', 'icon-cancel');
-	})
-	.done(function (response) {
-		// Remove the spinning icon.
-		icon.removeAttribute('style');
-
-		if (response.data)
+		if (option == 'com_config' && component == false && extension == false)
 		{
-			// Check if everything is OK
-			if (response.data.result == true)
-			{
-				icon.setAttribute('class', 'icon-save');
-
-				var ele = document.getElementsByTagName('td');
-				var next = ele.nextSibling;
-				document.getElementById(event.target).next.querySelector("span")
-					.removeClass()
-					.addClass(response['data']['class'])
-					.html(response.data.text);
+			asset = 'root.1';
+		}
+		else if (extension == false && view == 'component'){
+			asset = component;
+		}
+		else if (context){
+			if (view == 'group') {
+				asset = context + '.fieldgroup.' + Joomla.getUrlParam('id');
 			}
+			else {
+				asset = context + '.field.' + Joomla.getUrlParam('id');
+			}
+			title = document.getElementById('jform_title').value;
+		}
+		else if (extension != false && view != false){
+			asset = extension + '.' + view + '.' + Joomla.getUrlParam('id');
+			title = document.getElementById('jform_title').value;
+		}
+		else if (extension == false && view != false){
+			asset = option + '.' + view + '.' + Joomla.getUrlParam('id');
+			title = document.getElementById('jform_title').value;
 		}
 
-		// Render messages, if any. There are only message in case of errors.
-		if (typeof response.messages == 'object' && response.messages !== null)
-		{
-			Joomla.renderMessages(response.messages);
+		var id = this.id.replace('jform_rules_', '');
+		var lastUnderscoreIndex = id.lastIndexOf('_');
 
-			if (response.data && response.data.result == true)
-			{
-				icon.setAttribute('class', 'icon-save');
-			}
-			else
-			{
-				icon.setAttribute('class', 'icon-cancel');
-			}
+		var permission_data = {
+			comp   : asset,
+			action : id.substring(0, lastUnderscoreIndex),
+			rule   : id.substring(lastUnderscoreIndex + 1),
+			value  : value,
+			title  : title
+		};
 
-			window.scrollTo(0, 0);
-		}
-	});
-}) (event, Joomla);
+		// Remove js messages, if they exist.
+		Joomla.removeMessages();
+
+		// doing ajax request
+		Joomla.request({
+			method: 'POST',
+			url: document.getElementById('permissions-sliders').getAttribute('data-ajaxuri'),
+			data: permission_data,
+			perform: true,
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+
+			onError: function (xhr) {
+				// Remove the spinning icon.
+				icon.removeAttribute('style');
+
+				Joomla.renderMessages(Joomla.ajaxErrorsMessages(xhr));
+
+				window.scrollTo(0, 0);
+
+				icon.setAttribute('class', 'fa fa-times');
+			},
+			onSuccess: function (response, xhr) {
+				// Remove the spinning icon.
+				icon.removeAttribute('style');
+				response = JSON.parse(response);
+
+				if (response.data) {
+					// Check if everything is OK
+					if (response.data.result == true) {
+						icon.setAttribute('class', 'fa fa-check');
+						var next = event.target.parentElement.nextElementSibling;
+						next.removeChild(next.querySelector('span')).classList.add(response['data']['class']).html(response.data.text);
+					}
+				}
+
+				// Render messages, if any. There are only message in case of errors.
+				if (typeof response.messages == 'object' && response.messages !== null) {
+					Joomla.renderMessages(response.messages);
+
+					if (response.data && response.data.result == true) {
+						icon.setAttribute('class', 'fa fa-check');
+					}
+					else {
+						icon.setAttribute('class', 'fa fa-times');
+					}
+
+					window.scrollTo(0, 0);
+				}
+			}
+		});
+	}
+})(Joomla);
+
 
 /**
  * Function to get parameters out of the url
  */
-function getUrlParam(variable) {
+Joomla.getUrlParam = function(variable) {
 	var query = window.location.search.substring(1);
-	var vars = query.split('&');
-	for (var i=0;i<vars.length;i++)
-	{
+	var vars  = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
 		var pair = vars[i].split('=');
-		if (pair[0] == variable)
-		{
+		if (pair[0] == variable) {
 			return pair[1];
 		}
 	}
 	return false;
-}
+};
+
