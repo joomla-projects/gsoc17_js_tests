168124461898d540338ce17aa981a39a90263a47
Supun94 <supunpramoda@gmail.com>
2017-09-05 19:41:10 +0530

    Updating toolabbr unit tests

---
+++ tests/javascript/toolbar/spec-setup.js
@@ -0,0 +1,16 @@
+/**
+ * @package     Joomla.Tests
+ * @subpackage  JavaScript Tests
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * @since       3.6.3
+ * @version     1.0.0
+ */
+
+define(['jquery', 'text!testsRoot/toolbar/fixtures/fixture.html', 'libs/toolbar'], function ($, fixture) {
+	$('body').append(fixture);
+	
+	toolbarvalue = {mypage: 'toolbarpage', myname: 'toolbar', w: 10, h: 20, scroll: 'value'};
+});
